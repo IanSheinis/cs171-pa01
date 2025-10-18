c61ce8dea81c9e003ec58a359fc48c720593cbce
IanSheinis <iansheinis@gmail.com>
2025-10-17 17:03:17 -0700

    Changed comments

--- clock.ts
+++ clock.ts
@@ -12,12 +12,21 @@ export class Clock {
         this.rho = rho;
     } 
 
+    /**
+     * Getter for local time
+     * @returns local time
+     */
     public getTime() {
         const currentTime = utcTimeFnNumber();  // R(t)
         const elapsed = currentTime - this.lastSynchronizationTime;  // R(t) - Rbase
         return this.localTime + elapsed * (1 + this.rho);  // Lbase + elapsed × (1 + ρ), from lecture
     }
 
+    /**
+     * 
+     * @param newLocalTime Time for local clock to sync to
+     * Sync clock
+     */
     public setTime(newLocalTime: number) {
         this.localTime = newLocalTime;             
         this.lastSynchronizationTime = utcTimeFnNumber(); 
